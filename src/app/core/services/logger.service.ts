1356d06bf851aeb4d64e3a125cbb564cb6c12eec
rteissier <rteissier@laprovence.com>
2022-03-04 12:42:24 +0100

    refactor(configuration): Add logger to getUrl method

--- src/app/core/services/logger.service.ts
+++ src/app/core/services/logger.service.ts
@@ -2,26 +2,34 @@ import { environment } from 'src/environments/environment'
 import { Injectable } from '@angular/core'
 
 interface ILoggerService {
-    info<T>(message: string, ...args: T[]): void
-    log<T>(message: string, ...args: T[]): void
-    warn<T>(message: string, ...args: T[]): void
+  info<T>(message: string, ...args: T[]): void
+  log<T>(message: string, ...args: T[]): void
+  warn<T>(message: string, ...args: T[]): void
 }
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class LoggerService implements ILoggerService {
-    public info<T>(message: string, ...args: T[]): void {
-        console.info(message, ...args)
-    }
+  public info<T>(message: string, ...args: T[]): void {
+    console.info(message, ...args)
+  }
 
-    public log<T>(message: string, ...args: T[]): void {
-        if (!environment.production) {
-            console.log(message, ...args)
-        }
+  public log<T>(message: string, ...args: T[]): void {
+    if (!environment.production) {
+      console.log(message, ...args)
     }
+  }
 
-    public warn<T>(message: string, ...args: T[]): void {
-        console.warn(message, ...args)
-    }
+  public warn<T>(message: string, ...args: T[]): void {
+    console.warn(message, ...args)
+  }
+
+  /**
+   * Dummy implementation for sake of brevity,
+   * you should forward the error to a proper error handler.
+   */
+  public error<T>(message: string, ...args: T[]): void {
+    console.error(message, ...args)
+  }
 }
