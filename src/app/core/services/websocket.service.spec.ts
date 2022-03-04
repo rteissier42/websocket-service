2a30fd1c258cf8f9e1405a068c324b3b51c49634
rteissier <rteissier@laprovence.com>
2022-03-04 18:37:25 +0100

    feat(websocket): Implements WebSocket service

---
+++ src/app/core/services/websocket.service.spec.ts
@@ -0,0 +1,25 @@
+import { configurationFixture } from 'src/tests/fixtures/configuration.fixture'
+import { TestBed } from '@angular/core/testing'
+import { AppConfigService } from './app-config.service'
+
+import { WebSocketService } from './websocket.service'
+
+describe('WebsocketService', () => {
+  let webSocketService: WebSocketService
+  let appConfSpy: jasmine.SpyObj<AppConfigService>
+
+  beforeEach(() => {
+    appConfSpy = jasmine.createSpyObj<AppConfigService>('AppConf', ['getUrl'])
+    appConfSpy.getUrl.and.returnValue(
+      configurationFixture.urls.webSocketEndpointUrl
+    )
+    TestBed.configureTestingModule({
+      providers: [{ provide: AppConfigService, useValue: appConfSpy }],
+    })
+    webSocketService = TestBed.inject(WebSocketService)
+  })
+
+  it('should be created', () => {
+    expect(webSocketService).toBeTruthy()
+  })
+})
